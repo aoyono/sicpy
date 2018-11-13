bd53a2d4f2155f5c6cef36f0a696b194404606c2
Adrien Oyono <eikichife@yahoo.fr>
2018-11-13 22:03:26 +0100

    creates the theme generic_arithmetic_operations and exercise 2_77

---
+++ Chapter2/exercises/exercise2_77.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+https://mitpress.mit.edu/sicp/full-text/book/book-Z-H-16.html#%_thm_2.77
+"""
+from Chapter2.themes.data_directed_programming_and_additivity import (
+    angle, attach_tag, imag_part, install_rectangular_package, magnitude,
+    real_part
+)
+from Chapter2.themes.generic_arithmetic_operations import (
+    make_complex_from_real_imag
+)
+from Chapter2.themes.lisp_list_structured_data import lisp_list
+from Chapter2.themes.symbolic_data import quote
+from utils import get, let, put
+
+
+def install_complex_package():
+    # Imported procedures from rectangular and polar packages
+    def make_from_real_imag(x, y):
+        return get(
+            quote('make-from-real-imag'),
+            quote('rectangular'))(x, y)
+
+    def make_from_mag_ang(r, a):
+        return get(quote('make-from-mag-ang'), quote('polar'))(r, a)
+
+    # Internal procedures
+    def add_complex(z1, z2):
+        return make_from_real_imag(
+            real_part(z1) + real_part(z2),
+            imag_part(z1) + imag_part(z2))
+
+    def sub_complex(z1, z2):
+        return make_from_real_imag(
+            real_part(z1) - real_part(z2),
+            imag_part(z1) - imag_part(z2))
+
+    def mul_complex(z1, z2):
+        return make_from_mag_ang(
+            magnitude(z1) * magnitude(z2),
+            angle(z1) + angle(z2))
+
+    def div_complex(z1, z2):
+        return make_from_mag_ang(
+            magnitude(z1) / magnitude(z2),
+            angle(z1) - angle(z2))
+
+    # Interface to the rest of the system
+    def tag(z):
+        return attach_tag(quote('complex'), z)
+
+    put(quote('add'),
+        quote(lisp_list('complex', 'complex')),
+        lambda z1, z2: tag(add_complex(z1, z2)))
+    put(quote('sub'),
+        quote(lisp_list('complex', 'complex')),
+        lambda z1, z2: tag(sub_complex(z1, z2)))
+    put(quote('mul'),
+        quote(lisp_list('complex', 'complex')),
+        lambda z1, z2: tag(mul_complex(z1, z2)))
+    put(quote('div'),
+        quote(lisp_list('complex', 'complex')),
+        lambda z1, z2: tag(div_complex(z1, z2)))
+
+    put(quote('make-from-real-imag'),
+        quote('complex'),
+        lambda x, y: tag(make_from_real_imag(x, y)))
+    put(quote('make-from-mag-ang'),
+        quote('complex'),
+        lambda r, a: tag(make_from_mag_ang(r, a)))
+
+    put(quote('real-part'),
+        quote(lisp_list('complex')),
+        real_part)
+    put(quote('imag-part'),
+        quote(lisp_list('complex')),
+        imag_part)
+    put(quote('magnitude'),
+        quote(lisp_list('complex')),
+        magnitude)
+    put(quote('angle'),
+        quote(lisp_list('complex')),
+        angle)
+    return quote('done')
+
+
+def run_the_magic():
+    print("""
+        Procedure calls chain when calling (magnitude z):
+        (magnitude z) => (apply-generic 'magnitude z)
+        (apply-generic 'magnitude z) => (apply magnitude '((contents z)))
+        (apply magnitude '((contents z))) => (magnitude (contents z))
+        (magnitude (contents z)) => (apply-generic 'magnitude (contents z))
+        (apply-generic 'magnitude (contents z)) => (apply magnitude '((
+        contents (contents z))))
+        (apply magnitude '((contents (contents z)))) => (magnitude (contents
+        (contents z))
+        The last magnitude procedure is the one defined in the installation
+        procedure of rectangular package
+    """)
+    install_rectangular_package()
+    install_complex_package()
+    with let(make_complex_from_real_imag(3, 4)) as (z,):
+        print(
+            '(install-rectangular-package)\n(install-complex-package)\n(let ((z'
+            ' (make-complex-from-real-imag 3 4)))\n\t(magnitude z))',
+            magnitude(z),
+            sep='\n')
+
+
+if __name__ == "__main__":
+    run_the_magic()
---
+++ Chapter2/themes/generic_arithmetic_operations.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+from Chapter1.themes.great_common_divisor import gcd
+from Chapter2.themes.data_directed_programming_and_additivity import \
+    apply_generic
+from Chapter2.themes.lisp_list_structured_data import car, cdr, cons, lisp_list
+from Chapter2.themes.data_directed_programming_and_additivity import (
+    angle, attach_tag, imag_part, magnitude, real_part,
+)
+from Chapter2.themes.symbolic_data import quote
+from utils import get, let, put
+
+
+def add(x, y):
+    return apply_generic(quote('add'), x, y)
+
+
+def sub(x, y):
+    return apply_generic(quote('sub'), x, y)
+
+
+def mul(x, y):
+    return apply_generic(quote('mul'), x, y)
+
+
+def div(x, y):
+    return apply_generic(quote('div'), x, y)
+
+
+def install_scheme_number_package():
+    def tag(x):
+        return attach_tag(quote('scheme-number'), x)
+
+    put(quote('add'),
+        quote(lisp_list('scheme-number', 'scheme-number')),
+        lambda x, y: tag(x + y))
+    put(quote('sub'),
+        quote(lisp_list('scheme-number', 'scheme-number')),
+        lambda x, y: tag(x - y))
+    put(quote('mul'),
+        quote(lisp_list('scheme-number', 'scheme-number')),
+        lambda x, y: tag(x * y))
+    put(quote('div'),
+        quote(lisp_list('scheme-number', 'scheme-number')),
+        lambda x, y: tag(x / y))
+    put(quote('make'),
+        quote('scheme-number'),
+        lambda x: tag(x))
+    return quote('done')
+
+
+def make_scheme_number(n):
+    return get(quote('make'), quote('scheme-number'))(n)
+
+
+def install_rational_package():
+    # Internal procedures
+    def numer(x):
+        return car(x)
+
+    def denom(x):
+        return cdr(x)
+
+    def make_rat(num, den):
+        """The IF block comes from exercise 2.1"""
+        with let(gcd(num, den)) as (g,):
+            if num * den < 0:
+                return cons(
+                    - abs(num) // g,
+                    abs(den) // g)
+            return cons(
+                abs(num) // g,
+                abs(den) // g)
+
+    def add_rat(x, y):
+        return make_rat(
+            numer(x) * denom(x) + numer(y) * denom(y),
+            denom(x) * denom(y))
+
+    def sub_rat(x, y):
+        return make_rat(
+            numer(x) * denom(x) - numer(y) * denom(y),
+            denom(x) * denom(y))
+
+    def mul_rat(x, y):
+        return make_rat(
+            numer(x) * numer(y),
+            denom(x) * denom(y))
+
+    def div_rat(x, y):
+        return make_rat(
+            numer(x) * denom(y),
+            denom(x) * numer(y))
+
+    # interface to the rest of the system
+    def tag(x):
+        return attach_tag(quote('rational'), x)
+
+    put(quote('add'),
+        quote(lisp_list('rational', 'rational')),
+        lambda x, y: tag(add_rat(x, y)))
+    put(quote('sub'),
+        quote(lisp_list('rational', 'rational')),
+        lambda x, y: tag(sub_rat(x, y)))
+    put(quote('mul'),
+        quote(lisp_list('rational', 'rational')),
+        lambda x, y: tag(mul_rat(x, y)))
+    put(quote('div'),
+        quote(lisp_list('rational', 'rational')),
+        lambda x, y: tag(div_rat(x, y)))
+
+    put(quote('make'),
+        quote('rational'),
+        lambda n, d: tag(make_rat(n, d)))
+    return quote('done')
+
+
+def make_rational(n, d):
+    return get(quote('make'), quote('rational'))(n, d)
+
+
+def install_complex_package():
+    # Imported procedures from rectangular and polar packages
+    def make_from_real_imag(x, y):
+        return get(quote('make-from-real-imag'), quote('rectangular'))(x, y)
+
+    def make_from_mag_ang(r, a):
+        return get(quote('make-from-mag-ang'), quote('polar'))(r, a)
+
+    # Internal procedures
+    def add_complex(z1, z2):
+        return make_from_real_imag(
+            real_part(z1) + real_part(z2),
+            imag_part(z1) + imag_part(z2))
+
+    def sub_complex(z1, z2):
+        return make_from_real_imag(
+            real_part(z1) - real_part(z2),
+            imag_part(z1) - imag_part(z2))
+
+    def mul_complex(z1, z2):
+        return make_from_mag_ang(
+            magnitude(z1) * magnitude(z2),
+            angle(z1) + angle(z2))
+
+    def div_complex(z1, z2):
+        return make_from_mag_ang(
+            magnitude(z1) / magnitude(z2),
+            angle(z1) - angle(z2))
+
+    # Interface to the rest of the system
+    def tag(z):
+        return attach_tag(quote('complex'), z)
+
+    put(quote('add'),
+        quote(lisp_list('complex', 'complex')),
+        lambda z1, z2: tag(add_complex(z1, z2)))
+    put(quote('sub'),
+        quote(lisp_list('complex', 'complex')),
+        lambda z1, z2: tag(sub_complex(z1, z2)))
+    put(quote('mul'),
+        quote(lisp_list('complex', 'complex')),
+        lambda z1, z2: tag(mul_complex(z1, z2)))
+    put(quote('div'),
+        quote(lisp_list('complex', 'complex')),
+        lambda z1, z2: tag(div_complex(z1, z2)))
+
+    put(quote('make-from-real-imag'),
+        quote('complex'),
+        lambda x, y: tag(make_from_real_imag(x, y)))
+    put(quote('make-from-mag-ang'),
+        quote('complex'),
+        lambda r, a: tag(make_from_mag_ang(r, a)))
+    return quote('done')
+
+
+def make_complex_from_real_imag(x, y):
+    return get(quote('make-from-real-imag'),
+               quote('complex'))(x, y)
+
+
+def make_complex_from_mag_ang(r, a):
+    return get(quote('make-from-mag-ang'),
+               quote('complex'))(r, a)
+
+
+def run_the_magic():
+    pass
+
+
+if __name__ == '__main__':
+    run_the_magic()
